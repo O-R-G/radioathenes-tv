91c586298eac7bace7f7262583c6119711789824
Wei-Hao Wang <wwang03@risd.edu>
2020-07-30 15:36:49 -0400

    cleanup

--- views/home.php
+++ views/home.php
@@ -229,21 +229,10 @@
   
 })();
 var body = document.body;
-var isFullscreen = false;
 window.addEventListener('resize', function(){
-  console.log('resized!');
-  console.log(window.innerWidth);
-  console.log(window.innerHeight);
   if(window.innerWidth > window.innerHeight)
     body.requestFullscreen();
   else
     Document.exitFullscreen();
-});
-body.addEventListener('click', function(){
-  if(!isFullscreen)
-    body.requestFullscreen();
-  else
-    Document.exitFullscreen();
-  isFullscreen = !isFullscreen;
 });
 </script>
